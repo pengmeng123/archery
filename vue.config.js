e9372af4dfd1c95d38d53019489e5507693396ad
pengmeng <1575170534@qq.com>
2021-07-20 23:11:11 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   productionSourceMap: false,
+  chainWebpack: (config) => {
+    const imagesRule = config.module.rule("images");
+    imagesRule
+      .use("url-loader")
+      .loader("url-loader")
+      .tap((options) => Object.assign(options, { limit: 6144 }));
+  },
   devServer: {
     disableHostCheck: true,
     proxy: {
